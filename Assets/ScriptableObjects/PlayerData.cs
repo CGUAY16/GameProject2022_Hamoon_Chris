b64c5bebcb5bcd8c3af89981cd069cfc2a74deb3
Chris G <rmgchris@hotmail.com>
2022-02-19 12:19:26 -0500

    Test Scene Setup/base setup for character(not done)
    This is a commit to send to hamoon for starting his own test scene.
    it has my test scene, it contains placeholder tilemap assets for use. Character controller isnt done yet.

---
+++ Assets/ScriptableObjects/PlayerData.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* This is where we store data about the player
+ * so that we can access this data outside the 
+ * scene. 
+ * 
+ */
+
+public class PlayerData : ScriptableObject
+{
+    private void Awake()
+    {
+        
+    }
+
+
+}
