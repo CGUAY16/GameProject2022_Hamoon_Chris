7f5a1abb19c95ccc6e6f2da1e04ce1405e44b6e8
Chris G <rmgchris@hotmail.com>
2022-02-27 12:11:55 -0500

    Project Save for fixing .gitignore file
    I am saving my project onto here to go fix the .gitignore file in the master branch.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private PlayerControls _playerControls;
+
+    private void OnEnable()
+    {
+        //_playerControls.MainProtag.Movement.
+
+    }
+
+    private void OnDisable()
+    {
+        //_playerControls.MainProtag.Movement.
+    }
+
+    private void Awake()
+    {
+        
+    }
+
     // Start is called before the first frame update
     void Start()
     {
